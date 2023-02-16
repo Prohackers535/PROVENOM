d2b77400d99c8fd5a5134cd7bf3155ddd97d7b66
Prohackers535 <102033124+Prohackers535@users.noreply.github.com>
2023-02-16 14:11:54 +0530

    Create fixmetaerror.py

---
+++ fixmetaerror.py
@@ -0,0 +1,24 @@
+import subprocess
+import time
+import colorama
+from colorama import Fore
+
+print ("")
+print ("")
+
+subprocess.call("figlet " + "PROVENOM" + " | lolcat", shell=True)
+
+time.sleep(3)
+
+subprocess.call("clear", shell=True)
+print(Fore.GREEN + "Coded By:- Abdul Fasith")
+
+subprocess.call("figlet " + "PRO HACKERS" + " | lolcat", shell=True)
+
+print(Fore.BLUE + "-*-*-*-*-*-*-*-*-*- PRO HACKERS -*-*-*-*-*-*-*-*-*-")
+print("")
+print(Fore.RED + " ☆ Github==> https://github.com/Prohackers535")
+print("")
+
+subprocess.call("apt update && apt upgrade -y && cd $PREFIX/opt/metasploit-framework && bundle install", shell=True)
+subprocess.call("cd", shell=True)
